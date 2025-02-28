36e9b1545f3af590ed136c5d34d43f2de66c7e44
sajjad rahman <67529599+sajjadrahman56@users.noreply.github.com>
2025-02-28 10:31:11 +0600

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,7 +1,7 @@
 from azure.ai.projects import AIProjectClient
 from azure.identity import DefaultAzureCredential
 
-project_connection_string = " "
+project_connection_string = "eastus2.api.azureml.ms;2b46222c-47e2-4629-adad-85b2706566cc;rg-mdsajjadurrahman-8601_ai;mdsajjadurrahman-8310"
 
 project = AIProjectClient.from_connection_string(
     conn_str=project_connection_string, credential=DefaultAzureCredential()
